2e8cb0ed2abdfc34d59795ec3bca50882e2c5cf9
Joel Anderson <joelr.anderson@parivedasolutions.com>
2015-10-05 22:46:48 -0500

    Added validation. Added bootstrap.

--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specfy whether you'll attend")]
         public bool? WillAttend { get; set; }
     }
 }
