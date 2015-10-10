3d5595ed0a284c4c50b8d21771ef411ca98d8591
Joel Anderson <joelr.anderson@parivedasolutions.com>
2015-10-10 14:04:45 -0500

    Added new project. Also created inital Razor project

---
+++ LanguageFeatures/LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart : IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable<Product>)Products).GetEnumerator();
+        }
+
+        IEnumerator<Product> IEnumerable<Product>.GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+    }
+}
